e9a2ba85517517eb2cf6342b4c171874d3cbdb85
Aditi Shetty <aditi.shetty@detroitlabs.com>
2014-11-06 08:26:21 -0500

    Added onReceive method for widget

--- app/src/main/java/com/detroitlabs/trafficapp/InfoWidgetIntentReceiver.java
+++ app/src/main/java/com/detroitlabs/trafficapp/InfoWidgetIntentReceiver.java
@@ -3,6 +3,7 @@ package com.detroitlabs.trafficapp;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.RemoteViews;
 
 /**
  * Created by aditishetty on 11/6/14.
@@ -13,6 +14,10 @@ public class InfoWidgetIntentReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        
+        if (intent.getAction().equals(WIDGET_UPDATE_ACTION)) {
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.traffic_info_widget);
+
+            TrafficInfoWidgetProvider.pushWidgetUpdate(context, views);
+        }
     }
 }
