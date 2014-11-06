ad3363441150b23f95ea5376b55c3df0d2c2d52a
Aditi Shetty <aditi.shetty@detroitlabs.com>
2014-11-06 08:23:24 -0500

    Adding receiver class

---
+++ app/src/main/java/com/detroitlabs/trafficapp/InfoWidgetIntentReceiver.java
@@ -0,0 +1,18 @@
+package com.detroitlabs.trafficapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by aditishetty on 11/6/14.
+ */
+public class InfoWidgetIntentReceiver extends BroadcastReceiver {
+
+    final static String WIDGET_UPDATE_ACTION = "com.example.detroitlabs.intent.action.UPDATE_WIDGET";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        
+    }
+}
