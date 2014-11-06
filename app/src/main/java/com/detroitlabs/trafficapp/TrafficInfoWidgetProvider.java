88a57cf79aa7697363732cb6dbdcd8f70b9771be
Aditi Shetty <aditi.shetty@detroitlabs.com>
2014-11-06 08:38:42 -0500

    Adding another class

---
+++ app/src/main/java/com/detroitlabs/trafficapp/TrafficInfoWidgetProvider.java
@@ -0,0 +1,18 @@
+package com.detroitlabs.trafficapp;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+/**
+ * Created by aditishetty on 11/6/14.
+ */
+public class TrafficInfoWidgetProvider extends AppWidgetProvider {
+
+    final static String WIDGET_UPDATE_ACTION = "com.example.detroitlabs.intent.action.UPDATE_WIDGET";
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
+    }
+}
