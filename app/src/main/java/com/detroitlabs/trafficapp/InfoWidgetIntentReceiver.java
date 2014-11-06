cc4e2aa1a82f8e03d96458f1e2cb1f4a73a1a64c
Aditi Shetty <aditi.shetty@detroitlabs.com>
2014-11-06 08:54:11 -0500

    Added xml directory with xml widget layout

--- app/src/main/java/com/detroitlabs/trafficapp/InfoWidgetIntentReceiver.java
+++ app/src/main/java/com/detroitlabs/trafficapp/InfoWidgetIntentReceiver.java
@@ -1,7 +1,7 @@
 package com.detroitlabs.trafficapp;
 
 import android.content.BroadcastReceiver;
-import android.content.Context;
+import android.content.Context
 import android.content.Intent;
 import android.widget.RemoteViews;
 
