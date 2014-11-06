c1a54d1be59d4562a2d1e2a52189d9d503dafc9f
Andrew J B <ajbtee@gmail.com>
2014-11-06 13:13:07 -0500

    generic notification

--- app/src/main/java/com/detroitlabs/trafficapp/MainActivity.java
+++ app/src/main/java/com/detroitlabs/trafficapp/MainActivity.java
@@ -1,6 +1,11 @@
 package com.detroitlabs.trafficapp;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,6 +22,8 @@ public class MainActivity extends Activity {
         getFragmentManager().beginTransaction()
                 .add(R.id.container, new ListViewFragment())
                 .commit();
+
+        TrafficNotification.generate(this, "Hello World");
     }
 
 
